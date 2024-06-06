143e6c1fb6668bc1ff0878848bcb2983dbd68fb3
diamondniam <1yungdiamond@gmail.com>
2024-06-06 20:17:01 +0300

    fixes

--- src/components/MainFilters/Elements/SortSelectElement.vue
+++ src/components/MainFilters/Elements/SortSelectElement.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
   <div class="flex items-center gap-2">
-    <select class="text-blue-800 border-0 w-auto h-auto">
+    <select class="text-blue-800 border-0 w-auto h-auto outline-none">
       <slot></slot>
     </select>
     <img class="h-2 rotate-90 mr-2" src="../../../assets/right-arrow.png" alt="">
